22cf63f65b42e9dd36d0be75cc7b46c42c6b17c9
oyama <52624121+yoyama92@users.noreply.github.com>
2023-07-17 16:56:40 +0900

    週末カウントまで実装

---
+++ haskell/main.hs
@@ -0,0 +1,36 @@
+import Data.Time.Calendar (fromGregorian, gregorianMonthLength, toGregorian)
+import Data.Time.Calendar.OrdinalDate
+import Data.Time.Format (defaultTimeLocale, parseTimeM)
+import System.Environment (getArgs)
+
+isWeekEnd :: Day -> Bool
+isWeekEnd date
+  | dayOfWeek == 6 = True
+  | dayOfWeek == 7 = True
+  | otherwise = False
+  where
+    (_, dayOfWeek) = mondayStartWeek date
+
+parseYearMonth :: String -> Maybe Day
+parseYearMonth inputYearMonth = parseTimeM True defaultTimeLocale "%Y%m" inputYearMonth :: Maybe Day
+
+generateTargetMonthDays :: Integer -> Int -> [Day]
+generateTargetMonthDays targetYear targetMonth = [generateDay dayOfMonth | dayOfMonth <- daysOfMonth]
+  where
+    generateDay = fromGregorian targetYear targetMonth
+    monthLength = gregorianMonthLength targetYear targetMonth
+    daysOfMonth = [1 .. monthLength]
+
+countWeekend :: [Day] -> Int
+countWeekend dates = length [date | date <- dates, isWeekEnd date]
+
+countHoliday :: Maybe Day -> Int
+countHoliday Nothing = 0
+countHoliday (Just targetYearMonth) = countWeekend (generateTargetMonthDays targetYear targetMonth)
+  where
+    (targetYear, targetMonth, _) = toGregorian targetYearMonth
+
+main :: IO ()
+main = do
+  args <- getArgs
+  print $ countHoliday (parseYearMonth (head args))
